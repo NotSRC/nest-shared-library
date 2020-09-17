fa1be232cda83575a0c3c6fd352398eaee231d71
Dima Tsourkan <dimatsourkan@gmail.com>
2020-09-17 17:07:39 +0300

    0.0.19

--- libs/notification/src/notification.module.ts
+++ libs/notification/src/notification.module.ts
@@ -1,29 +1,66 @@
-import {DynamicModule, Module} from '@nestjs/common';
+import {DynamicModule, Module, ModuleMetadata, Provider, Type} from '@nestjs/common';
 import {NotificationService} from './notification.service';
 import {ClientsModule, Transport} from '@nestjs/microservices';
 import {NOTIFICATION_SERVICE_NAME} from './notification.constants';
 import {EmailNotificationService} from './email-notification/email-notification.service';
 
-@Module({
-  providers: [NotificationService, EmailNotificationService],
-  exports: [NotificationService, EmailNotificationService],
-})
+interface NotificationModuleOptions {
+  host: string;
+  port: number;
+}
+
+export interface NotificationModuleAsyncOptions
+  extends Pick<ModuleMetadata, 'imports'> {
+  useFactory?: (...args: any[]) => Promise<NotificationModuleOptions> | NotificationModuleOptions;
+  inject?: any[];
+}
+
+@Module({})
 export class NotificationModule {
-  static forRoot(host: string, port: number): DynamicModule {
+
+  static register(options: NotificationModuleOptions): DynamicModule {
     return {
+      global: true,
       module: NotificationModule,
+      providers: [NotificationService, EmailNotificationService],
+      exports: [NotificationService, EmailNotificationService],
       imports: [
         ClientsModule.register([
           {
             name: NOTIFICATION_SERVICE_NAME,
             transport: Transport.TCP,
             options: {
-              host: host,
-              port: port,
+              host: options.host,
+              port: options.port,
             },
           }
         ])
       ]
     };
   }
+
+  static async forRootAsync(asyncOptions: NotificationModuleAsyncOptions): Promise<DynamicModule> {
+
+    const options = await asyncOptions.useFactory(asyncOptions.inject);
+    return {
+      global: true,
+      module: NotificationModule,
+      providers: [NotificationService, EmailNotificationService, ...asyncOptions.inject],
+      exports: [NotificationService, EmailNotificationService],
+      imports: [
+        ...asyncOptions.imports,
+        ClientsModule.register([
+          {
+            name: NOTIFICATION_SERVICE_NAME,
+            transport: Transport.TCP,
+            options: {
+              host: options.host,
+              port: options.port,
+            },
+          }
+        ])
+      ],
+    };
+  }
+
 }
