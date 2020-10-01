4004069f985ecc364f1170738375a400586f87dd
Dima Tsourkan <dimatsourkan@gmail.com>
2020-10-01 14:01:41 +0300

    feat(App): Update paths

--- libs/comment/src/comment.service.ts
+++ libs/comment/src/comment.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { PaginateModel } from 'mongoose';
 import { Comment } from './shemas/comment.schema';
-import { CrudService, DmLoggerService } from '../..';
+import { CrudService } from '../../crud/crud.service';
+import { DmLoggerService } from '../../logger/src/logger.service';
 
 @Injectable()
 export class CommentService<T, D> extends CrudService<T, D> {
--- libs/user/src/user.service.ts
+++ libs/user/src/user.service.ts
@@ -3,7 +3,8 @@ import { InjectModel } from '@nestjs/mongoose';
 import { PaginateModel } from 'mongoose';
 import { User } from './shemas/user.schema';
 import { CreateUserDto } from './dto/create-user.dto';
-import { CrudService, DmLoggerService } from '../..';
+import { CrudService } from '../../crud/crud.service';
+import { DmLoggerService } from '../../logger/src/logger.service';
 
 @Injectable()
 export class UserService extends CrudService<CreateUserDto, any> {
