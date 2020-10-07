3317d9a2e35b6df976d6f30b5619b380109631d8
Dima Tsourkan <dimatsourkan@gmail.com>
2020-10-07 17:00:11 +0300

    feat(App): Add crud module

--- libs/index.ts
+++ libs/index.ts
@@ -1,6 +1,7 @@
 export * from './notification/src/index';
 export * from './logger/src/index';
 export * from './user/src/index';
+export * from './files-crud/src/index';
 export * from './filter/filter.service';
 export * from './crud/crud.service';
 export * from './dto/query.dto';
