b7ddd826a3594e6b77e7c54abe6b2fd05cf372b2
Dmitry Tsurkan <dtsurkan@deluxemodular.com>
2020-11-25 10:34:00 +0200

    feat(Crud): add calendar query

--- libs/logger/src/logger.interceptor.ts
+++ libs/logger/src/logger.interceptor.ts
@@ -6,16 +6,12 @@ import {
 } from '@nestjs/common';
 import { DmLoggerService } from './logger.service';
 import { catchError } from 'rxjs/operators';
-import { Observable } from 'rxjs';
 
 @Injectable()
 export class DmLoggerInterceptor implements NestInterceptor {
   constructor(private logger: DmLoggerService) {}
 
-  intercept(
-    context: ExecutionContext,
-    next: CallHandler,
-  ): Observable<any> | Promise<Observable<any>> {
+  intercept(context: ExecutionContext, next: CallHandler): any {
     return next.handle().pipe(
       catchError((exception) => {
         this.logger.captureException(exception);
