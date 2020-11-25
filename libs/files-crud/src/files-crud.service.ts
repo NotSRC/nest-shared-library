2c612ecb8abda723dbb80fe8e677e2a1b3d7f2f0
Dmitry Tsurkan <dtsurkan@deluxemodular.com>
2020-11-25 10:25:33 +0200

    feat(Crud): add calendar query

--- libs/files-crud/src/files-crud.service.ts
+++ libs/files-crud/src/files-crud.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PaginateModel } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
-import { StaticFileDocument, StaticFileModel } from './shemas/file.shema';
+import { StaticFileDocument } from './shemas/file.shema';
 import { CrudService } from '../../crud/crud.service';
 import { DmLoggerService } from '../../logger/src/logger.service';
 
 @Injectable()
-export class FilesCrudService extends CrudService<StaticFileModel> {
+export class FilesCrudService extends CrudService<StaticFileDocument> {
   constructor(
     @InjectModel('StaticFile')
     protected stateModel: PaginateModel<StaticFileDocument>,
