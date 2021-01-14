da0cfe362e50abd37acf996158a530a36780c18c
Dmitry Tsurkan <dtsurkan@deluxemodular.com>
2021-01-14 12:02:55 +0200

    feat(App): Added testing module

--- libs/logger/src/logger.service.ts
+++ libs/logger/src/logger.service.ts
@@ -3,40 +3,35 @@ import { SentryService } from '@ntegral/nestjs-sentry';
 
 @Injectable()
 export class DmLoggerService extends Logger {
-
   constructor(private sentryService: SentryService) {
     super();
   }
 
-  error(exception: Object|string, context?: string): void {
+  error(exception: Object | string, context?: string): void {
     try {
       this.captureException(exception, context);
       super.error(exception, null, context);
-    } catch (err) {  }
+    } catch (err) {}
   }
 
-  warn(exception: Object|string, trace?: string): void {
+  warn(exception: Object | string, trace?: string): void {
     try {
       this.captureException(exception);
       super.warn(exception, trace);
-    } catch (err) {  }
+    } catch (err) {}
   }
 
-  captureException(exception: Object|string, context?: string) {
-
+  captureException(exception: Object | string, context?: string) {
+    console.error(context, exception);
     let json;
     try {
-      json = JSON.stringify({context: context, exception});
+      json = JSON.stringify({ context: context, exception });
     } catch (e) {
       json = exception;
     }
 
     try {
       this.sentryService.instance().captureException(json);
-    } catch (e) {
-      
-    }
+    } catch (e) {}
   }
 }
-
-
