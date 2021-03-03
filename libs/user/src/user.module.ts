2429c251b110f4958814f7cc81b5d654247796f1
Dmitry Tsurkan <dtsurkan@deluxemodular.com>
2021-03-03 17:18:42 +0200

    feat(User): Updated user schema

--- libs/user/src/user.module.ts
+++ libs/user/src/user.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule, Schema } from '@nestjs/mongoose';
+import { MongooseModule, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 import { UserProvider } from './shemas/user.schema';
 import { UserService } from './user.service';
@@ -7,16 +7,13 @@ import { UserService } from './user.service';
 @Schema()
 export class Location {}
 
+const LocationSchema = SchemaFactory.createForClass(Location);
+
 @Module({
   providers: [UserService],
   imports: [
-    MongooseModule.forFeatureAsync([
-      UserProvider,
-      {
-        name: 'Location',
-        useFactory: () => Location,
-      },
-    ]),
+    MongooseModule.forFeature([{ name: 'Location', schema: LocationSchema }]),
+    MongooseModule.forFeatureAsync([UserProvider]),
   ],
   exports: [UserService],
 })
